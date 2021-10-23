ff6f3a9c789c82d2660ea4b1143ecdf53b35a2d2
pgm-kilidebo <kilian.debock@student.arteveldehs.be>
2021-10-23 14:04:25 +0200

    Lyttle Development: New images, changes to the headers and main.css, optimisations and name changes to about to  community bot.

---
+++ test.js
@@ -0,0 +1,21 @@
+const array = [
+  [ //list 1
+    'L1-item1',
+    'L1-item2'
+  ],
+  [ //list 2
+    'L2-item1',
+    'L2-item2'
+  ]
+];
+// list index 0, index 0
+console.log(array[0][0]);
+
+// list index 0, index 1
+console.log(array[0][1]);
+
+// list index 1, index 0
+console.log(array[1][0]);
+
+// list index 1, index 1
+console.log(array[1][1]);
